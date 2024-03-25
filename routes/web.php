92e1edde345e7ca15619e01cc597ae5fa186293d
Moses Asiago <mozzeago@gmail.com>
2024-03-25 09:22:49 +0300

    controls edit

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\SectionController;
+use App\Http\Controllers\ControlController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 Route::get('/sections', [SectionController::class, 'index'])->name('sections');
+Route::get('/controls', [ControlController::class, 'index'])->name('controls');
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
