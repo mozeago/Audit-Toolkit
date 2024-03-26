0629b3d2532c4b03f6fc5d05e92b3bc7fbe42f3a
Moses Asiago <mozzeago@gmail.com>
2024-03-26 17:53:19 +0300

    Update create.blade.php

--- resources/views/livewire/questions/create.blade.php
+++ resources/views/livewire/questions/create.blade.php
@@ -1,74 +1,6 @@
 <?php
 
 use Livewire\Volt\Component;
-<<<<<<< HEAD
-<<<<<<< HEAD
-use App\Models\Control;
-use App\Models\Question;
-use Livewire\Attributes\Validate;
-
-new class extends Component {
-    public $controls = [];
-    public string $text = '';
-    public string $control_id = '';
-    public function rules()
-    {
-        return [
-            'text' => 'required|min:20',
-            'control_id' => 'required|uuid',
-        ];
-    }
-    public function mount()
-    {
-        $this->controls = Control::all();
-    }
-    public function store(): void
-    {
-        if (empty($this->control_id)) {
-            $this->addError('control_id', 'Please select a control.');
-            return;
-        }
-        $validatedData = $this->validate();
-        $question = Question::create([
-            'text' => $validatedData['text'],
-            'control_id' => $validatedData['control_id'],
-        ]);
-        $this->dispatch('question-created');
-        $this->resetFields();
-    }
-    public function resetFields()
-    {
-        $this->reset('text', 'control_id');
-    }
-}; ?>
-
-<div>
-    <form wire:submit.prevent="store">
-        <div class="flex flex-col mb-4 space-y-4">
-            <textarea wire:model="text" rows="5" placeholder="{{ __('Question Text') }}"
-                class="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-primary-500 focus:ring-opacity-50"></textarea>
-            @error('text')
-                <span class="mt-2 text-xs text-red-500">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="flex flex-col w-full mb-4">
-            <select wire:model="control_id" id="control_id"
-                class="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-primary-500 focus:ring-opacity-500">
-                <option value=""disabled selected>Select...</option>
-                @foreach ($controls as $control)
-                    <option value="{{ $control->id }}">{{ $control->name }}</option>
-                @endforeach
-
-            </select>
-            @error('control_id')
-                <span class="mt-2 text-xs text-red-500">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-6">
-            <x-primary-button type="submit">{{ __('Save') }}</x-primary-button>
-        </div>
-    </form>
-=======
 use App\Models\Question;
 use App\Models\Control;
 use Illuminate\Database\Eloquent\Collection;
@@ -107,46 +39,6 @@ new class extends Component {
     }
 }; ?>
 
-=======
-use App\Models\Question;
-use App\Models\Control;
-use Illuminate\Database\Eloquent\Collection;
-new class extends Component {
-    public Collection $controls;
-    public $questionText = '';
-    public $controlId = '';
-    public function mount()
-    {
-        $this->fetchControls();
-    }
-    public function rules()
-    {
-        return [
-            'questionText' => 'required|min:20',
-            'controlId' => 'required|uuid',
-        ];
-    }
-    public function fetchControls()
-    {
-        $this->controls = Control::orderBy('created_at', 'desc')->get();
-    }
-    public function store()
-    {
-        $validatedData = $this->validate();
-        $question = Question::create([
-            'text' => $validatedData['questionText'],
-            'control_id' => $validatedData['controlId'],
-        ]);
-        $this->dispatch('question-created');
-        $this->resetFields();
-    }
-    public function resetFields()
-    {
-        return $this->reset('questionText', 'controlId');
-    }
-}; ?>
-
->>>>>>> questions-crud
 <div class="flex">
     <div class="w-full max-w-md min-w-full px-8 py-6 leading-normal bg-white rounded">
         <form wire:submit.prevent="store">
@@ -180,8 +72,4 @@ new class extends Component {
             </div>
         </form>
     </div>
-<<<<<<< HEAD
->>>>>>> questions-crud
-=======
->>>>>>> questions-crud
 </div>
