9ce907637e9c0a2dcd2b00d270ee01e6d3a4c2c9
Moses Asiago <mozzeago@gmail.com>
2024-04-10 21:09:25 +0300

    templates download complete

--- resources/views/livewire/templatesdownload/templatesdownload.blade.php
+++ resources/views/livewire/templatesdownload/templatesdownload.blade.php
@@ -18,22 +18,41 @@ new class extends Component {
     }
 }; ?>
 
-<div class="w-full">
-    <div class="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+<div class="w-full pl-5 mt-10 mb-10">
+    <div class="grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-3">
         @foreach ($templates as $template)
-            <div class="relative overflow-hidden bg-white rounded-lg shadow-md">
-                <div class="h-40 bg-center bg-cover"
-                    style="background-image: url('{{ Storage::url($template->thumbnail) }}')" aria-hidden="true"></div>
-                <div class="absolute inset-0 flex flex-col items-center justify-center p-2 bg-gray-500 opacity-70">
-                    <div class="text-center">
-                        <h5 class="mb-2 text-lg font-bold text-black">{{ $template->name }}</h5>
-                        <p class="mb-2 text-sm text-black">{{ $template->category }}</p>
-                    </div>
-                    <div class="flex justify-center mt-auto">
+            <div class="relative max-w-sm mx-auto mb-20 ml-2 mr-2 overflow-hidden bg-white shadow-lg rounded-xl">
+                <img class="object-cover w-full h-64" src="{{ Storage::url($template->thumbnail) }}" alt="Image"
+                    style="opacity: 0.5;">
+                <button class="absolute top-0 right-0 px-2 py-1 mt-2 mr-2 text-white bg-gray-800 rounded">
+                    {{ $template->category }}
+                </button>
+                <div class="p-6">
+                    <div class="mb-2 text-xl font-bold">{{ $template->name }}</div>
+                    <p class="mb-4 text-base text-gray-700">{{ $template->category }}</p>
+                    <div class="flex justify-between">
                         <a href="{{ Storage::url($template->url) }}" target="_blank"
-                            class="inline-block px-4 py-2 ml-2 font-semibold transition duration-300 border rounded-full shadow-md text-custom-red bg-primary border-custom-red hover:bg-opacity-80 hover:border-opacity-80 hover:border-custom-red">Preview</a>
-                        <a href="{{ Storage::url($template->url) }}" download
-                            class="inline-block px-4 py-2 ml-2 font-semibold transition duration-300 border rounded-full shadow-md text-custom-red bg-secondary border-custom-red hover:bg-opacity-80 hover:border-opacity-80 hover:border-custom-red">Download</a>
+                            class="inline-flex items-center px-4 py-2 font-bold text-gray-800 bg-gray-200 rounded hover:bg-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                            </svg>
+                            Preview
+                        </a>
+                        <a href="{{ Storage::url($template->url) }}" download>
+                            <button
+                                class="inline-flex items-center px-4 py-2 font-bold text-gray-800 bg-gray-200 rounded hover:bg-red-800 hover:text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none"
+                                    stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                                    <polyline points="7 10 12 15 17 10" />
+                                    <line x1="12" y1="15" x2="12" y2="3" />
+                                </svg>
+                                Download
+                            </button>
+                        </a>
                     </div>
                 </div>
             </div>
