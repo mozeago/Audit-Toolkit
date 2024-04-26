b68da714f92bb87fa8af7655491fe4e94c36346f
Moses Asiago <mozzeago@gmail.com>
2024-04-26 14:08:33 +0300

    bg on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,15 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
-    <div class="mx-auto overflow-hidden bg-white shadow-sm sm:rounded-lg">
+    </x-slot> --}}
+    <div style="background-image: url({{ asset('images/bg-questionnaire.jpeg') }});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 100%;"
+        class="mx-auto overflow-hidden bg-gray-100 shadow-sm sm:rounded-lg">
         <livewire:dashboard.userchart />
     </div>
 </x-app-layout>
