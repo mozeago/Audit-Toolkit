93dd9be9562c993b6400221521e40b3c5266fc21
Moses Asiago <mozzeago@gmail.com>
2024-05-02 08:50:24 +0300

    landing page redesign

---
+++ resources/views/livewire/common/topnav.blade.php
@@ -0,0 +1,24 @@
+<nav class="sticky top-0 z-50 bg-white shadow-lg">
+    <div class="px-4 mx-auto max-w-7xl">
+        <div class="flex items-center justify-between py-4">
+            <!-- Logo -->
+            <div class="flex-shrink-0">
+                <image src="{{ asset('images/logo-red.PNG') }}" class="h-8" alt="logo" />
+            </div>
+            <!-- Navigation Links -->
+            <div class="hidden space-x-4 md:flex">
+                {{-- <a href="#" class="text-gray-800 hover:text-gray-900">Home</a>
+                <a href="#" class="text-gray-800 hover:text-gray-900">About</a>
+                <a href="#" class="text-gray-800 hover:text-gray-900">Services</a> --}}
+                @if (Route::currentRouteName() !== 'register')
+                    <a href="{{ route('register') }}"
+                        class="text-gray-800 hover:text-gray-900 hover:hover:text-[#C8000B]">Register</a>
+                @endif
+                @if (Route::currentRouteName() !== 'login')
+                    <a href="{{ route('login') }}"
+                        class="text-gray-800 hover:text-gray-900 hover:hover:text-[#C8000B]">Sign in</a>
+                @endif
+            </div>
+        </div>
+    </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,26 +11,55 @@
     <style>
         body {
             font-family: 'Roboto', sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        .container {
+            max-width: 1100px;
+            margin: 0 auto;
+            padding: 0 20px;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #000;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        .btn:hover {
+            background-color: #333;
+        }
+
+        .hero {
+            background-color: #C8000B;
+            color: #fff;
+        }
+
+        .feature {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            transition: transform 0.3s ease;
+        }
+
+        .feature:hover {
+            transform: translateY(-5px);
         }
     </style>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="antialiased">
-    <header class="py-4 text-white bg-black">
-        <div class="container flex items-center justify-between px-4 mx-auto">
-            <div class="logo">
-                <img src="{{ asset('images/Scartch and Script Logo white.svg') }}" alt="Company Logo">
-            </div>
-            <nav>
-                <ul class="flex space-x-8">
-                    <li><a href="{{ route('login') }}" class="px-4 py-2">Login</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <livewire:common.topnav />
 
-    <section class="py-20 px-8 text-white bg-[#C8000B] hero">
+    <section class="px-8 py-20 hero">
         <div class="container mx-auto text-center">
             <h1 class="text-4xl font-bold">Data Protection Toolkit 2024</h1>
             <p class="mt-4 text-lg">The Data Protection Toolkit 2024 offers a cutting-edge solution for organizations
@@ -47,9 +76,7 @@
                 Scratch and Script Limited is proud to collaborate with partners and young professionals across Africa,
                 including:
             </p>
-            <a href="{{ route('questionnaire') }}"
-                class="inline-block px-6 py-2 mt-8 text-white bg-black rounded btn hover:text-gray-300">Get
-                Started</a>
+            <a href="{{ route('questionnaire') }}" class="mt-8 btn">Get Started</a>
         </div>
     </section>
 
@@ -57,57 +84,56 @@
         <div class="container mx-auto">
             <h2 class="mb-8 text-3xl font-bold text-center">Our Partners</h2>
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-5">
-                <div class="p-4 bg-white rounded-lg shadow-md feature">
-                    <img src="{{ asset('images/isacakenya.jpeg') }}" alt="Image"
-                        class="object-cover w-full h-24 mb-2 rounded-lg">
+                <div class="p-4 feature">
+                    <img src="{{ asset('images/isacakenya.jpeg') }}" alt="ISACA Kenya"
+                        class="w-full h-24 mb-2 rounded-lg">
                     <h3 class="mb-2 text-lg font-bold">ISACA Kenya</h3>
                     <p class="text-sm">A leading professional association for IT governance, risk management, and
                         cybersecurity professionals in Kenya.</p>
                 </div>
-                <!-- Repeat similar structure for other cards -->
-                <div class="p-4 bg-white rounded-lg shadow-md feature">
-                    <img src="{{ asset('images/kampala.jpeg') }}" alt="Image"
-                        class="object-cover w-full h-24 mb-2 rounded-lg">
-                    <h3 class="mb-2 text-lg font-bold">ISACA Kampala Chapter</h3>
+                <div class="p-4 feature">
+                    <img src="{{ asset('images/kampala.jpg') }}" alt="ISACA Kenya" class="w-full h-24 mb-2 rounded-lg">
+                    <h3 class="mb-2 text-lg font-bold">ISACA Kampala</h3>
                     <p class="text-sm">Dedicated to promoting IT governance, risk management, and cybersecurity
                         practices in Uganda.</p>
                 </div>
-                <div class="p-4 bg-white rounded-lg shadow-md feature">
-                    <img src="{{ asset('images/rwanda.jpeg') }}" alt="Image"
-                        class="object-cover w-full h-24 mb-2 rounded-lg">
-                    <h3 class="mb-2 text-lg font-bold">3. ISACA Rwanda Chapter</h3>
+                <div class="p-4 feature">
+                    <img src="{{ asset('images/rwanda.jpg') }}" alt="ISACA Kenya" class="w-full h-24 mb-2 rounded-lg">
+                    <h3 class="mb-2 text-lg font-bold">ISACA Rwanda</h3>
                     <p class="text-sm">Committed to advancing IT governance and cybersecurity knowledge in Rwanda.</p>
                 </div>
-                <div class="p-4 bg-white rounded-lg shadow-md feature">
-                    <img src="{{ asset('images/sa.jpeg') }}" alt="Image"
-                        class="object-cover w-full h-24 mb-2 rounded-lg">
-                    <h3 class="mb-2 text-lg font-bold">ISACA South Africa Chapter</h3>
+                <div class="p-4 feature">
+                    <img src="{{ asset('images/ISACA_South_Africa.jpeg') }}" alt="ISACA Kenya"
+                        class="w-full h-24 mb-2 rounded-lg">
+                    <h3 class="mb-2 text-lg font-bold">ISACA South Africa</h3>
                     <p class="text-sm">A key organization driving IT governance and cybersecurity awareness in South
                         Africa.</p>
                 </div>
-                <div class="p-4 bg-white rounded-lg shadow-md feature">
-                    <img src="{{ asset('images/kenya.jpeg') }}" alt="Image"
-                        class="object-cover w-full h-24 mb-2 rounded-lg">
+                <div class="p-4 feature">
+                    <img src="{{ asset('images/kenya data privacy.jpeg') }}" alt="ISACA Kenya"
+                        class="w-full h-24 mb-2 rounded-lg">
                     <h3 class="mb-2 text-lg font-bold">Data Privacy and Governance Society of Kenya</h3>
-                    <p class="text-sm">An association advocating for data privacy rights and promoting best practices in
+                    <p class="text-sm">An association advocating for data privacy rights and promoting best
+                        practices in
                         data governance in Kenya.</p>
                 </div>
             </div>
         </div>
     </section>
-    <section class="px-8 py-16 bg-white">
+
+    <section class="px-8 py-16 bg-white researchers">
         <div class="container mx-auto">
             <h2 class="mb-8 text-3xl font-bold text-center">Researchers</h2>
-            <p class="px-4 mb-8 text-lg leading-relaxed text-gray-800 md:px-0">
+            <p class="px-4 mb-8 text-lg leading-relaxed text-gray-800">
                 In addition to our partners, we're supported by young professionals across Africa. They're researching
                 and analyzing data protection laws, providing valuable insights. They're also actively learning about
                 data protection, improving their understanding of regulations and best practices. Their dedication is
                 vital in our mission to make data protection accessible to all and create a safer online world.
             </p>
-            <p class="px-4 mb-8 text-lg leading-relaxed text-gray-800 md:px-0">
+            <p class="px-4 mb-8 text-lg leading-relaxed text-gray-800">
                 <strong>Program Researchers</strong>
             </p>
-            <ol class="px-4 mb-8 text-lg leading-relaxed text-gray-800 list-decimal list-inside md:px-0">
+            <ol class="px-4 mb-8 text-lg leading-relaxed text-gray-800 list-decimal list-inside">
                 <!-- Dummy list of 20 members -->
                 <li class="mb-4">
                     <span class="font-semibold text-purple-600">Beatrice Mwangi</span>
@@ -120,22 +146,14 @@
                 <!-- Repeat similar structure for other list items -->
                 <!-- Add more researchers if needed -->
             </ol>
-            <p class="px-4 text-lg leading-relaxed text-gray-800 md:px-0">
+            <p class="px-4 text-lg leading-relaxed text-gray-800">
                 Together, let's work towards a safer online environment where data privacy
                 rights are respected and upheld. Thank you for joining us on this journey.
             </p>
         </div>
     </section>
 
-
-    <footer class="py-8 text-center text-white bg-black">
-        <p>&copy; 2024 Scratch & Script Limited</p>
-        <ul class="mt-4 social-links">
-            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-        </ul>
-    </footer>
+    <livewire:common.footer />
 </body>
 
 </html>
