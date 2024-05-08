2c0eb0db69a6cf30e6fd598da6d25385a2f60462
Moses Asiago <mozzeago@gmail.com>
2024-05-08 18:54:41 +0300

    removed alpine add via link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -434,7 +434,6 @@
     {{-- footer --}}
     {{-- end footer --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
 </body>
 
 </html>
