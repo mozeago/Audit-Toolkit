607d1384b126df8028246ee2942aca661e58edd9
Moses Asiago <mozzeago@gmail.com>
2024-05-22 23:53:46 +0300

    naviagtion responsive for mobile

--- resources/views/livewire/common/topnav.blade.php
+++ resources/views/livewire/common/topnav.blade.php
@@ -1,33 +1,39 @@
 <nav class="sticky top-0 z-50 bg-white shadow-lg">
-    <div class="flex items-center justify-between px-4 py-3 mx-auto max-w-7xl">
+    <div
+        class="mx-auto flex max-w-7xl items-center justify-between px-4 py-2">
         <!-- Logo -->
-        <div class="flex-shrink-0">
+        <div class="mr-2 w-1/3 flex-shrink-0 sm:w-auto">
             <a href="/" wire:navigate>
-                <img src="{{ asset('images/scratchandscript_logo_red.webp') }}" class="h-8"
+                <img src="{{ asset('images/scratchandscript_logo_red.webp') }}"
+                    class="h-8"
                     alt="scratch and script logo" />
             </a>
         </div>
         <!-- Navigation Links -->
-        <div class="flex items-center space-x-2">
+        <div
+            class="flex w-2/3 items-center justify-end space-x-2 sm:ml-auto sm:w-auto">
             @guest
                 <button type="button"
-                    class="hover:text-[#C8000B] sm:px-1 px-4 py-1 border-2 rounded-md border-gray-500 text-black font-bold sm:font-normal hover:border-[#C8000B]">
-                    <a href="{{ route('register') }}" class="text-gray-800 no-underline hover:text-[#C8000B]">Register</a>
+                    class="md:min-w-28 flex-grow rounded-md border-2 border-gray-500 px-4 py-1 font-bold text-black hover:border-[#C8000B] hover:text-[#C8000B] sm:flex-grow-0 sm:px-1 sm:font-normal">
+                    <a href="{{ route('register') }}"
+                        class="text-gray-800 no-underline hover:text-[#C8000B]">Register</a>
                 </button>
             @endguest
             @guest
                 <button type="button"
-                    class="hover:text-white hover:bg-green-700 hover:border-green-700 sm:px-1 px-4 py-1 border-2 rounded-md border-[#C8000B] bg-[#C8000B] text-white font-bold sm:font-normal">
-                    <a href="{{ route('login') }}" class="text-white no-underline">Sign in</a>
+                    class="md:min-w-28 lg:min-w-28 xl:min-w-28 2xl:min-w-28 flex-grow rounded-md border-2 border-[#C8000B] bg-[#C8000B] px-4 py-1 font-bold font-bold text-white hover:border-[#F6FCFC] hover:bg-black hover:text-white sm:flex-grow-0 sm:px-1 sm:font-normal">
+                    <a href="{{ route('login') }}"
+                        class="text-white no-underline">Sign
+                        in</a>
                 </button>
             @endguest
             @auth
                 <button type="button"
-                    class="hover:text-[#C8000B] px-4 py-1 border-2 rounded-md border-gray-300 text-black font-bold hover:border-[#C8000B]">
-                    <a href="{{ route('dashboard') }}" class="text-gray-800 no-underline">Dashboard</a>
+                    class="md:min-w-28 lg:min-w-28 xl:min-w-28 2xl:min-w-28 flex-grow rounded-md border-2 border-gray-300 px-4 py-1 font-bold font-bold text-black hover:border-[#C8000B] hover:text-[#C8000B] sm:flex-grow-0">
+                    <a href="{{ route('dashboard') }}"
+                        class="text-gray-800 no-underline">Dashboard</a>
                 </button>
             @endauth
         </div>
     </div>
-
 </nav>
