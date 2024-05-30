6090903e6987f013fbb9f94a4fe5ea25d853b4d8
Moses Asiago <mozzeago@gmail.com>
2024-05-30 12:12:14 +0300

    migration to delete silently responses tables

--- database/migrations/2024_05_30_085322_drop_responses_tables.php
+++ database/migrations/2024_05_30_085322_drop_responses_tables.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::dropIfExists('user_responses');
         Schema::dropIfExists('risk_analysis_responses');
+        DB::statement("
+            DELETE FROM migrations
+            WHERE migration IN ('2024_03_27_185338_create_user_responses_table', '2024_04_07_203129_create_risk_analysis_responses_table');
+        ");
     }
 
     /**
