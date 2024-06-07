6e4bf02de3cfc67db9d107aaf37a66f18cbc1a89
Moses Asiago <mozzeago@gmail.com>
2024-06-07 15:04:40 +0300

    audit subsection,onboarding questions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,9 +55,11 @@
             display: none !important;
         }
     </style>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </head>
 
-<body x-data="{ auditOpened: false, onBoardingOpened: false }">
+<body x-data="{ auditOpened: false, onBoardingOpened: false, securityQuestionsOpened: false }">
     <?php
     use App\Models\User;
     ?>
@@ -86,8 +88,8 @@
                     <h3 class="m-0">Dashboard</h3>
                 </a>
                 @if (auth()->user()->role === 'admin')
-                    <a onclick="return false;" @click="auditOpened = !auditOpened" href="#"
-                        class="active:text-[#C8000B] hover:font-semibold hover:ml-0">
+                    <a data-dropdown-toggle="auditDropdown" onclick="return false;" @click="auditOpened = !auditOpened"
+                        href="#" class="active:text-[#C8000B] hover:font-semibold hover:ml-0">
                         <span class="material-icons-sharp">
                             handyman
                         </span>
@@ -96,6 +98,53 @@
                             expand_more
                         </span>
                     </a>
+                    <div id="auditDropdown"
+                        class="z-30 hidden w-auto pr-4 font-normal bg-white border-2 border-gray-200 rounded-lg shadow-md">
+                        <ul class="items-start py-2 pt-8 text-sm text-gray-700">
+                            <li>
+                                <a href="{{ route('controls') }}"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        roundabout_left
+                                    </span>Controls
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('sections') }}"
+                                    class="flex items-start px-2 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="mr-2 material-icons-sharp">
+                                        repartition
+                                    </span>
+                                    Sections
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('recommendations') }}"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        recommend
+                                    </span>Recommendations
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('information') }}"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        tips_and_updates
+                                    </span>Information
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('questions') }}"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        quiz
+                                    </span>Questions
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
                     <a href="{{ route('templates-upload') }}" class="active:text-[#C8000B] hover:font-semibold">
                         <span class="material-icons-sharp">
                             upload_file
@@ -142,13 +191,71 @@
                         </span>
                         <h3>System Users</h3>
                     </a>
+                    {{-- <a data-dropdown-toggle="securityQuestionsDropdown" onclick="return false;"
+                        @click="securityQuestionsOpened = !securityQuestionsOpened" href="#"
+                        class="active:text-[#C8000B] hover:font-semibold hover:ml-0">
+                        <span class="material-icons-sharp">
+                            question_mark
+                        </span>
+                        <h3>Security Qtns</h3>
+                        <span :class="{ 'rotate-180': securityQuestionsOpened }" class="material-icons-sharp">
+                            expand_more
+                        </span>
+                    </a>
+                    <div id="securityQuestionsDropdown"
+                        class="z-30 hidden w-auto pr-4 font-normal bg-white border-2 border-[#F97316]/5 rounded-lg shadow-md">
+                        <ul class="items-start py-2 pt-8 text-sm text-gray-700">
+                            <li>
+                                <a href="#"
+                                    class="flex items-start px-2 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="mr-2 material-icons-sharp">
+                                        repartition
+                                    </span>
+                                    Sections
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        domain
+                                    </span>Sub sections
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        recommend
+                                    </span>Recommendations
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        tips_and_updates
+                                    </span>Information
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        quiz
+                                    </span>Questions
+                                </a>
+                            </li>
+                        </ul>
+                    </div> --}}
                     <a href="{{ route('privacy-cases') }}" class="active:text-[#C8000B] hover:font-semibold">
                         <span class="material-icons-sharp">
                             phonelink_lock
                         </span>
                         <h3>Privacy Cases</h3>
                     </a>
-                    <a onclick="return false;" @click="onBoardingOpened = !onBoardingOpened" href="#"
+                    <a data-dropdown-toggle="onBoardingDropdown" onclick="return false;"
+                        @click="onBoardingOpened = !onBoardingOpened" href="#"
                         class="active:text-[#C8000B] hover:font-semibold hover:ml-0">
                         <span class="material-icons-sharp">
                             question_mark
@@ -158,6 +265,44 @@
                             expand_more
                         </span>
                     </a>
+                    <div id="onBoardingDropdown"
+                        class="z-30 hidden w-auto pr-4 font-normal bg-white border-2 border-[#F97316]/50 rounded-lg shadow-md">
+                        <ul class="items-start py-2 pt-8 text-sm text-gray-700">
+                            <li>
+                                <a href="{{ route('risk-analysis-section') }}"
+                                    class="flex items-start px-2 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="mr-2 material-icons-sharp">
+                                        repartition
+                                    </span>
+                                    Sections
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('risk-analysis-recommendation') }}"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        recommend
+                                    </span>Recommendations
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('risk-analysis-information') }}"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        tips_and_updates
+                                    </span>Information
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('risk-analysis-subsection') }}"
+                                    class="flex items-start px-4 py-2 hover:font-semibold hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <span class="material-icons-sharp">
+                                        quiz
+                                    </span>Questions
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 @endif
             </div>
         </aside>
