e91e8507ab3480ac39448f90157a3f79fa5ddae5
Moses Asiago <mozzeago@gmail.com>
2024-06-08 16:31:54 +0300

    templated download, histroy data

--- resources/views/livewire/templatesdownload/templatesdownload.blade.php
+++ resources/views/livewire/templatesdownload/templatesdownload.blade.php
@@ -17,16 +17,81 @@ new class extends Component {
         return Template::orderBy('created_at', 'desc')->get();
     }
 }; ?>
-<div class="grid grid-cols-1 gap-4 md:grid-cols-3">
-    <div class="relative overflow-hidden bg-white rounded shadow-lg">
-        <img class="object-cover w-full h-48" src="https://via.placeholder.com/300" alt="Card Image">
-        <div class="px-6 py-4">
-            <div class="mb-2 text-xl font-bold">Card Title 1</div>
-            <p class="text-base text-gray-700">Card Subtitle 1</p>
+<div class="grid grid-cols-1 gap-4 text-black mt-14 md:grid-cols-3">
+    @foreach ($templates as $template)
+        <div class="flex flex-col max-w-sm overflow-hidden bg-white border border-gray-200 shadow-lg rounded-xl">
+            <img class="w-full rounded-t-xl" src="{{ Storage::url($template->thumbnail) }}" alt="{{ $template->name }}">
+            <div class="flex-grow px-6 py-4">
+                <div class="mb-2 text-xl font-bold">
+                    {{ $template->name }}</div>
+                <p class="text-base text-gray-700">
+                    {{ $template->category }}
+                </p>
+            </div>
+            <div class="flex items-center justify-between px-4 pt-4 pb-2 border-t border-gray-200">
+                <span class="text-gray-700"> <a href="{{ Storage::url($template->url) }}" target="_blank"
+                        class="inline-flex items-center flex-shrink px-1 py-2 text-gray-800 bg-gray-200 rounded hover:bg-black hover:text-white">
+                        <span class="mr-2 material-icons-sharp">
+                            local_library
+                        </span>
+                        <span class="text-sm font-light">Preview</span>
+                    </a></span>
+                <span class="text-gray-700">
+                    <a href="{{ Storage::url($template->url) }}" download>
+                        <button
+                            class="inline-flex items-center flex-grow flex-shrink px-1 py-2 font-bold text-gray-800 bg-gray-200 rounded hover:bg-red-800 hover:text-white">
+                            <span class="material-icons-sharp">
+                                download_for_offline
+                            </span>
+                            <span class="text-sm font-light">Download</span>
+                        </button>
+                    </a>
+                </span>
+            </div>
         </div>
-        <div class="absolute bottom-0 w-full bg-gray-200 py-2 px-4">
-            <p class="text-gray-800 text-sm">Fixed Row at Bottom</p>
-        </div>
-    </div>
-
+        {{-- <div
+            class="relative overflow-visible bg-white border-2 shadow-xl rounded-xl">
+            <img class="object-cover w-full h-48"
+                src="https://via.placeholder.com/300"
+                alt="{{ $template->title }}">
+            <button
+                class="absolute top-0 right-0 px-2 py-1 mt-2 mr-2 text-white bg-gray-800 rounded">
+                {{ $template->category }}
+            </button>
+            <div
+                class="mb-2 font-bold sm:text-sm md:text-xl">
+                Title</div>
+            <div
+                class="absolute bottom-0 left-0 right-0 p-4 bg-white">
+                <div class="grid gap-2 sm:grid-cols-2">
+                    <div class="flex w-full">
+                        <a href="{{ Storage::url($template->url) }}"
+                            target="_blank"
+                            class="inline-flex items-center flex-shrink px-1 py-2 text-black bg-gray-300 rounded hover:bg-black hover:text-white">
+                            <span
+                                class="mr-2 material-icons-sharp">
+                                local_library
+                            </span>
+                            <span
+                                class="text-sm font-light">Preview</span>
+                        </a>
+                    </div>
+                    <div class="flex w-full">
+                        <a href="{{ Storage::url($template->url) }}"
+                            download>
+                            <button
+                                class="inline-flex items-center flex-grow flex-shrink px-1 py-2 font-bold text-black bg-gray-300 rounded hover:bg-red-800 hover:text-white">
+                                <span
+                                    class="material-icons-sharp">
+                                    download_for_offline
+                                </span>
+                                <span
+                                    class="text-sm font-light">Download</span>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+    @endforeach
 </div>
