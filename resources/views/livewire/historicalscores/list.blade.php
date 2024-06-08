c94b77d833da5e3a2aca55166d122492ac8046ad
Moses Asiago <mozzeago@gmail.com>
2024-06-08 16:20:52 +0300

    templates download

--- resources/views/livewire/historicalscores/list.blade.php
+++ resources/views/livewire/historicalscores/list.blade.php
@@ -11,7 +11,7 @@ new class extends Component {
     public function mount()
     {
         $this->userId = auth()->user()->id;
-        $this->previousScores = $this->getPreviousScores();
+        // $this->previousScores = $this->getPreviousScores();
     }
     public function getPreviousScores()
     {
@@ -80,25 +80,24 @@ new class extends Component {
 
 <div class="max-w-sm">
     <div
-        class="p-6 overflow-y-auto transition-transform duration-300 ease-in-out transform bg-white rounded-lg shadow-lg max-h-96 hover:scale-105 hover:shadow-2xl focus-within:shadow-2xl focus-within:bg-gray-800">
+        class="max-h-96 transform overflow-y-auto rounded-lg bg-white p-6 shadow-lg transition-transform duration-300 ease-in-out focus-within:bg-gray-800 focus-within:shadow-2xl hover:scale-105 hover:shadow-2xl">
 
-        @if (!empty($previousScores))
+        {{-- @if (!empty($previousScores))
             @foreach ($previousScores as $previousScore)
                 <div class="notification">
                     <span
-                        class="mr-4 flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full
-                @if ($previousScore['average_score'] >= 75) bg-green-500
+                        class="@if ($previousScore['average_score'] >= 75) bg-green-500
                 @elseif ($previousScore['average_score'] >= 50)
                     bg-black
                 @else
-                    bg-[#C8000B] @endif
-                p-2 text-white">{{ $previousScore['average_score'] }}%</span>
+                    bg-[#C8000B] @endif mr-4 flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full p-2 text-white">{{ $previousScore['average_score'] }}%</span>
                     <div class="content">
                         <div class="info">
                             <h3>
                                 @if ($previousScore['average_score'] >= 75)
                                     Low Risk
-                                @elseif ($previousScore['average_score'] >= 50)
+                                    @elseif
+                                    ($previousScore['average_score'] >= 50)
                                     Moderate Risk
                                 @else
                                     High Risk
@@ -119,12 +118,13 @@ new class extends Component {
                 {{-- <span class="material-icons-sharp">
                     warning
                 </span>
-                <p class="font-mono">You don't have previous attempts yet.</p>
              --}}
-                <span class="material-icons-sharp">
-                    notifications_none
-                </span>
-            </div>
-        @endif
+        <span class="material-icons-sharp">
+            notifications_none
+        </span>
+        <p class="font-mono">You don't have previous
+            attempts yet.</p>
     </div>
+    {{-- @endif --}}
+</div>
 </div>
