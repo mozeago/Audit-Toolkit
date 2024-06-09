dd2d6c0b15817b454af24ae7d3ac92f4d325145d
Moses Asiago <mozzeago@gmail.com>
2024-06-09 22:42:35 +0300

    graduates list display

--- resources/views/livewire/researchers/list.blade.php
+++ resources/views/livewire/researchers/list.blade.php
@@ -27,87 +27,88 @@ new class extends Component {
     <p class="text-lg font-semibold text-black">Meet Our IGNITE Cohort 1 Graduates (i| DPO)</p>
 
     <div class="flex justify-center mt-8 mb-4 ">
-        <div class="flex-auto max-w-xs bg-gray-200 rounded-sm md:max-w-md">
-            <!-- Content for first column -->
-            <div class="px-4 py-2 mb-4 text-white bg-[#EEA726] rounded-t-sm">
-                <h3 class="text-lg font-bold">Ignite Cohort 1</h3>
-            </div>
-            <div class="items-start flex-auto bg-gray-200">
-                <ul class="items-start p-4 shadow-md">
-                    @foreach ($yellowColumContributors as $contributor)
-                        <li class="flex items-start py-2">
-                            <span
-                                class="flex items-start justify-center flex-shrink-0 w-8 h-8 mr-4 text-white rounded-full">
-                                <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                </svg>
-
-                            </span>
-                            <span class="text-gray-800">{{ $contributor->name }}</span>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="px-4 py-2 mt-auto text-white bg-[#EEA726] rounded-b-lg">
-                <h3 class="text-lg font-bold">Graduates</h3>
-            </div>
-        </div>
-        <div class="flex-auto max-w-xs mx-2 bg-gray-200 rounded-sm md:max-w-md">
-            <!-- Content for second column -->
-            <div class="px-4 py-2 mb-4 text-white bg-[#459BBA] rounded-t-sm">
-                <h3 class="text-lg font-bold">Ignite Cohort 1</h3>
-            </div>
-            <div class="flex-auto bg-gray-200">
-                <ul class="p-4 shadow-md">
-                    @foreach ($blueColumContributors as $contributor)
-                        <li class="flex items-start py-2">
-                            <span
-                                class="flex items-center justify-center flex-shrink-0 w-8 h-8 mr-4 text-white rounded-full">
-                                <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                </svg>
-
-                            </span>
-                            <span class="text-gray-800">{{ $contributor->name }}</span>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="px-4 py-2 mt-auto text-white bg-[#459BBA] rounded-b-lg">
-                <h3 class="text-lg font-bold">Graduates</h3>
-            </div>
-        </div>
-        <div class="flex-auto max-w-xs mx-2 bg-gray-200 rounded-sm md:max-w-md">
-            <!-- Content for Third column -->
-            <div class="px-4 py-2 mb-4 text-white bg-[#78A87F] rounded-t-sm">
-                <h3 class="text-lg font-bold">Ignite Cohort 1</h3>
-            </div>
-            <div class="flex-auto bg-gray-200">
-                <ul class="p-4 shadow-md">
-                    @foreach ($greenColumContributors as $contributor)
-                        <li class="flex items-start py-2">
-                            <span
-                                class="flex items-center justify-center flex-shrink-0 w-8 h-8 mr-4 text-white rounded-full">
-                                <svg class="w-6 h-6 text-gray-800 " aria-hidden="true"
+        <div class="flex-auto max-w-xs mr-4 bg-gray-200 rounded-sm md:max-w-md">
+            <div class="flex flex-col justify-between max-w-sm bg-white rounded-lg shadow-lg">
+                <!-- Title Row -->
+                <div class="px-6 py-4 text-xl font-bold text-white bg-[#F97316] rounded-t-lg">
+                    Ignite Cohort 1
+                </div>
+                <div class="flex-grow px-6 py-4">
+                    <!-- List of Names -->
+                    <ul class="list-none">
+                        @foreach ($yellowColumContributors as $contributor)
+                            <li class="flex items-center mb-2">
+                                <svg class="w-6 h-6 mr-2 text-gray-800" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                                     viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                         stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                 </svg>
-
-                            </span>
-                            <span class="text-gray-800">{{ $contributor->name }}</span>
-                        </li>
-                    @endforeach
-                </ul>
-
+                                <span class="text-gray-700">{{ $contributor->name }}</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!-- Footer Row -->
+                <div class="px-6 py-4 text-xl font-bold text-white bg-[#F97316] rounded-b-lg">
+                    Graduates
+                </div>
             </div>
-            <div class="px-4 py-2 mt-auto text-white bg-[#78A87F] rounded-b-lg">
-                <h3 class="text-lg font-bold">Graduates</h3>
+        </div>
+        <div class="flex-auto max-w-xs mr-4 bg-gray-200 rounded-sm md:max-w-md">
+            <div class="flex flex-col justify-between max-w-sm bg-white rounded-lg shadow-lg">
+                <!-- Title Row -->
+                <div class="px-6 py-4 text-xl font-bold text-white bg-[#459BBA] rounded-t-lg">
+                    Ignite Cohort 1
+                </div>
+                <div class="flex-grow px-6 py-4">
+                    <!-- List of Names -->
+                    <ul class="list-none">
+                        @foreach ($blueColumContributors as $contributor)
+                            <li class="flex items-center mb-2">
+                                <svg class="w-6 h-6 mr-2 text-gray-800" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                    viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <span class="text-gray-700">{{ $contributor->name }}</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!-- Footer Row -->
+                <div class="px-6 py-4 text-xl font-bold text-white bg-[#459BBA] rounded-b-lg">
+                    Graduates
+                </div>
+            </div>
+        </div>
+        <div class="flex-auto max-w-xs bg-gray-200 rounded-sm md:max-w-md">
+            <div class="flex flex-col justify-between max-w-sm bg-white rounded-lg shadow-lg">
+                <!-- Title Row -->
+                <div class="px-6 py-4 text-xl font-bold text-white bg-[#78A87F] rounded-t-lg">
+                    Ignite Cohort 1
+                </div>
+                <div class="flex-grow px-6 py-4">
+                    <!-- List of Names -->
+                    <ul class="list-none">
+                        @foreach ($blueColumContributors as $contributor)
+                            <li class="flex items-center mb-2">
+                                <svg class="w-6 h-6 mr-2 text-gray-800" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                    viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <span class="text-gray-700">{{ $contributor->name }}</span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!-- Footer Row -->
+                <div class="px-6 py-4 text-xl font-bold text-white bg-[#78A87F] rounded-b-lg">
+                    Graduates
+                </div>
             </div>
         </div>
     </div>
