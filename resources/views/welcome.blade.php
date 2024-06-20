cc0e8c01dd3dd1231c0c48a5a0ca01ec7ffec2aa
Moses Asiago <mozzeago@gmail.com>
2024-06-21 01:19:59 +0300

    landing page edits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,83 +46,75 @@
             <span class="inline-block border-b-2 border-[#C8000B] pb-4">Partners</span>
             Worldwide
         </h1>
-        <div class="flex flex-wrap items-center justify-center pb-8 md:flex-nowrap">
-            <div class="flex items-center justify-center w-full sm:mb-2 sm:w-1/2 lg:w-auto lg:flex-grow md:flex-grow">
-                <a class="zoom-image" href="https://isacaabuja.org/">
-                    <img src="https://data-protection-toolkit.scratchandscript.com/images/isaca_abuja.webp"
-                        alt="Image 1" class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
-                </a>
-            </div>
-            <div class="flex items-center justify-center w-full sm:mb-2 sm:w-1/2 lg:w-auto lg:flex-grow md:flex-grow">
-                <a class="zoom-image" href="https://engage.isaca.org/kenyachapter/home">
-                    <img src="https://data-protection-toolkit.scratchandscript.com/images/isacakenya.webp"
-                        alt="Image 1" class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
-                </a>
-            </div>
-            <div class="flex items-center justify-center w-full sm:mb-2 sm:w-1/2 lg:w-auto lg:flex-grow md:flex-grow">
-                <a class="zoom-image" href="https://engage.isaca.org/southafricachapter/home">
-                    <img src="https://data-protection-toolkit.scratchandscript.com/images/ISACA_South_Africa.webp"
-                        alt="Image 1" class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
-                </a>
-            </div>
-            <div class="flex items-center justify-center w-full sm:mb-2 sm:w-1/2 lg:w-auto lg:flex-grow md:flex-grow">
-                <a class="zoom-image" href="https://engage.isaca.org/kampalachapter/home">
-                    <img src="https://data-protection-toolkit.scratchandscript.com/images/kampala.webp" alt="Image 1"
-                        class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
-                </a>
-            </div>
-            <div class="flex items-center justify-center w-full sm:mb-2 sm:w-1/2 lg:w-auto lg:flex-grow md:flex-grow">
-                <a class="zoom-image"
-                    href="https://twitter.com/DataGovProsKe?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
-                    <img src="https://data-protection-toolkit.scratchandscript.com/images/kenya data privacy.webp"
-                        alt="kenya data privacy" class="w-auto h-8 sm:mx-2 sm:h-12">
-                </a>
-            </div>
-            <div class="flex items-center justify-center w-full sm:mb-2 sm:w-1/3 lg:w-auto lg:flex-grow md:flex-grow">
-                <a class="zoom-image" href="https://engage.isaca.org/rwandachapter/home">
-                    <img src="https://data-protection-toolkit.scratchandscript.com/images/rwanda.webp" alt="Image 1"
-                        class="w-auto h-8 sm:mx-2 sm:h-12 sm:w-full">
-                </a>
-            </div>
-            <div class="flex items-center justify-center w-full sm:w-2/3 lg:w-auto lg:flex-grow md:flex-grow">
-                <a class="zoom-image" href="https://www.scratchandscript.com/">
-                    <img src="https://data-protection-toolkit.scratchandscript.com/images/scratchandscript_logo_red.webp"
-                        alt="Image 1" class="w-auto h-8 sm:mx-2 sm:h-12">
-                </a>
-            </div>
-            <div
-                class="flex items-center justify-center w-full sm:w-full sm:mt-2 lg:w-auto lg:flex-grow sm:p-4 md:flex-grow">
-                <a href="https://www.scratchandscript.com/user_contact" class="w-full lg:w-auto">
-                    <button
-                        class="flex items-center rounded bg-[#C8000B] text-white font-bold py-2 px-4  hover:bg-black hover:shadow-2xl">
-                        <!-- Icon on the left -->
-                        {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                            class="w-5 h-5 mr-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                        </svg> --}}
+        <div class="w-9/12 mx-auto">
+            <div class="flex">
+                <div class="mr-6 w-1/8">
+                    <a class="zoom-image" href="https://isacaabuja.org/">
+                        <img src="https://data-protection-toolkit.scratchandscript.com/images/isaca_abuja.webp"
+                            alt="Image 1" class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
+                    </a>
+                </div>
+                <div class="mr-6 w-1/8">
+                    <a class="zoom-image" href="https://engage.isaca.org/kenyachapter/home">
+                        <img src="https://data-protection-toolkit.scratchandscript.com/images/isacakenya.webp"
+                            alt="Image 1" class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
+                    </a>
+                </div>
+                <div class="mr-6 w-1/8">
+                    <a class="zoom-image" href="https://engage.isaca.org/southafricachapter/home">
+                        <img src="https://data-protection-toolkit.scratchandscript.com/images/ISACA_South_Africa.webp"
+                            alt="Image 1" class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
+                    </a>
+                </div>
+                <div class="mr-8 w-1/8">
+                    <a class="zoom-image" href="https://engage.isaca.org/kampalachapter/home">
+                        <img src="https://data-protection-toolkit.scratchandscript.com/images/kampala.webp"
+                            alt="Image 1" class="w-auto h-8 sm:w-full sm:mx-2 sm:h-12">
+                    </a>
+                </div>
+                <div class="mr-6 w-1/8">
+                    <a class="zoom-image"
+                        href="https://twitter.com/DataGovProsKe?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                        <img src="https://data-protection-toolkit.scratchandscript.com/images/kenya data privacy.webp"
+                            alt="kenya data privacy" class="w-auto h-8 sm:mx-2 sm:h-12">
+                    </a>
+                </div>
+                <div class="mr-6 w-1/8">
+                    <a class="zoom-image" href="https://engage.isaca.org/rwandachapter/home">
+                        <img src="https://data-protection-toolkit.scratchandscript.com/images/rwanda.webp"
+                            alt="Image 1" class="w-auto h-8 sm:mx-2 sm:h-12 sm:w-full">
+                    </a>
+                </div>
+                <div class="mr-6 w-1/8">
+                    <a class="zoom-image" href="https://www.scratchandscript.com/">
+                        <img src="https://data-protection-toolkit.scratchandscript.com/images/scratchandscript_logo_red.webp"
+                            alt="Image 1" class="w-auto h-8 sm:mx-2 sm:h-12">
+                    </a>
+                </div>
+                <div class="w-1/8">
+                    <a href="https://www.scratchandscript.com/user_contact"
+                        class="rounded bg-[#C8000B] text-white font-bold py-4 px-4  hover:bg-black hover:shadow-2xl flex items-center">
                         <svg class="w-10 h-4 sm:h-6 sm:w-16" fill="none" stroke="currentColor" viewBox="0 0 52 52"
                             xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
                                 d="M64,12.78v17s-3.63.71-4.38.81-3.08.85-4.78-.78C52.22,27.25,42.93,18,42.93,18a3.54,3.54,0,0,0-4.18-.21c-2.36,1.24-5.87,3.07-7.33,3.78a3.37,3.37,0,0,1-5.06-2.64,3.44,3.44,0,0,1,2.1-3c3.33-2,10.36-6,13.29-7.52,1.78-1,3.06-1,5.51,1C50.27,12,53,14.27,53,14.27a2.75,2.75,0,0,0,2.26.43C58.63,14,64,12.78,64,12.78ZM27,41.5a3,3,0,0,0-3.55-4.09,3.07,3.07,0,0,0-.64-3,3.13,3.13,0,0,0-3-.75,3.07,3.07,0,0,0-.65-3,3.38,3.38,0,0,0-4.72.13c-1.38,1.32-2.27,3.72-1,5.14s2.64.55,3.72.3c-.3,1.07-1.2,2.07-.09,3.47s2.64.55,3.72.3c-.3,1.07-1.16,2.16-.1,3.46s2.84.61,4,.25c-.45,1.15-1.41,2.39-.18,3.79s4.08.75,5.47-.58a3.32,3.32,0,0,0,.3-4.68A3.18,3.18,0,0,0,27,41.5Zm25.35-8.82L41.62,22a3.53,3.53,0,0,0-3.77-.68c-1.5.66-3.43,1.56-4.89,2.24a8.15,8.15,0,0,1-3.29,1.1,5.59,5.59,0,0,1-3-10.34C29,12.73,34.09,10,34.09,10a6.46,6.46,0,0,0-5-2C25.67,8,18.51,12.7,18.51,12.7a5.61,5.61,0,0,1-4.93.13L8,10.89v19.4s1.59.46,3,1a6.33,6.33,0,0,1,1.56-2.47,6.17,6.17,0,0,1,8.48-.06,5.4,5.4,0,0,1,1.34,2.37,5.49,5.49,0,0,1,2.29,1.4A5.4,5.4,0,0,1,26,34.94a5.47,5.47,0,0,1,3.71,4,5.38,5.38,0,0,1,2.39,1.43,5.65,5.65,0,0,1,1.48,4.89,0,0,0,0,1,0,0s.8.9,1.29,1.39a2.46,2.46,0,0,0,3.48-3.48s2,2.48,4.28,1c2-1.4,1.69-3.06.74-4a3.19,3.19,0,0,0,4.77.13,2.45,2.45,0,0,0,.13-3.3s1.33,1.81,4,.12c1.89-1.6,1-3.43,0-4.39Z">
                             </path>
                         </svg>
-                        Become a Partner
-                    </button>
-                </a>
+                        <span class="text-nowrap">Become a Partner</span>
+                    </a>
+                </div>
             </div>
-
         </div>
-
     </div>
     {{-- end partners --}}
     {{-- start report --}}
-    <div class="flex flex-col px-8 mt-8 mb-8 border-b-2 border-gray-100 shadow-sm">
-        <div class="container grid items-center justify-center grid-cols-12 p-4 mx-auto mb-4 sm:w-full">
+    <div class="flex flex-col px-32 mb-8 border-b-2 border-gray-100 shadow-sm">
+        <div class="container grid items-center justify-center grid-cols-12 p-4 mx-auto mb-2 sm:w-full">
             <div class="col-span-3 sm:col-span-4 md:block lg:block"></div>
             <span class="col-span-1 border-2 border-black">
             </span>
             <div class="col-span-6 text-xl font-bold text-center roboto-bold sm:col-span-2">
-                <span>Skills Report</span>
+                <span class="text-nowrap">Skills Report</span>
             </div>
             <span class="col-span-1 border-2 border-black">
             </span>
@@ -131,10 +123,10 @@
         <div class="flex w-full pb-8">
             <div class="flex flex-col flex-grow w-full sm:w-1/2">
                 <img src="{{ asset('images/Annual-report-ignite.webp') }}" alt="Skills Report"
-                    class="w-full fade-left-5 rounded-bl-xl rounded-tl-xl">
+                    class="w-full fade-left-5 rounded-bl-xl rounded-tl-xl max-h-96">
             </div>
             <div class="flex flex-col flex-grow w-1/2 py-0 pl-8">
-                <h2 class="mb-8 text-xl font-bold leading-snug roboto-bold">
+                <h2 class="mb-2 text-xl font-bold leading-snug roboto-bold">
                     <span class="inline-block pb-1 text-[#C8000B]">2024
                         SUB-SAHARA
                     </span>
@@ -142,7 +134,7 @@
                         AND DATA PROTECTION
                         SKILLS REPORT
                 </h2>
-                <p class="mt-2 roboto-regular-24">
+                <p class="roboto-regular-24">
                     In the IGNITE 2024 Sub-Sahara Learning &
                     Skills Trends Report, we delve into
                     these three key areas
@@ -172,30 +164,44 @@
     </div>
     {{-- end report --}}
     {{-- Start SDG focus --}}
-    <div class="flex flex-col justify-center px-8 py-8 mt-4 mb-16 border-b-2 border-gray-100">
+    <div class="flex flex-col justify-center px-32 py-8 mt-4 mb-16 border-b-2 border-gray-100">
         <h1 class="mb-8 text-xl font-bold roboto-bold text-start">
             Our <span class="inline-block border-b-2 border-[#C8000B] pb-1 text-[#C8000B]">SDG</span>
             Focus
         </h1>
-        <div class="flex w-full gap-2 justify-items-stretch">
-            <div class="flex items-center justify-center flex-1">
-                <a href="https://tinyurl.com/39975atx" class="flex items-center justify-center">
-                    <img src="{{ asset('images/Sustainable_Development_Goal_04QualityEducation.webp') }}"
-                        alt="Sustainable_Development_Goal_4_Quality_Education" class="rounded-md shadow-2xl">
-                </a>
+        <div class="grid grid-cols-12 gap-2">
+            <div class="col-span-2">
+                <div class="flex justify-center">
+                    <div class="flex items-center justify-center">
+                        <a href="https://tinyurl.com/39975atx" class="flex items-center justify-center">
+                            <img src="{{ asset('images/Sustainable_Development_Goal_04QualityEducation.webp') }}"
+                                alt="Sustainable_Development_Goal_4_Quality_Education" class="rounded-md shadow-2xl">
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="flex items-center justify-center flex-1">
-                <a href="https://tinyurl.com/33x8dmst">
-                    <img src="{{ asset('images/Sustainable_Development_Goal_05GenderEquality.webp') }}"
-                        alt="Sustainable_Development_Goal_5_Gender_Equality" class="rounded-md shadow-2xl">
+            <div class="col-span-2">
+                <div class="flex justify-center">
+                    <div class="flex items-center justify-center">
+                        <a href="https://tinyurl.com/33x8dmst">
+                            <img src="{{ asset('images/Sustainable_Development_Goal_05GenderEquality.webp') }}"
+                                alt="Sustainable_Development_Goal_5_Gender_Equality" class="rounded-md shadow-2xl">
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="flex items-center justify-center flex-1">
-                <a href="https://tinyurl.com/ycy9s5kn">
-                    <img src="{{ asset('images/Sustainable_Development_Goal_08DecentWork.webp') }}"
-                        alt="Sustainable_Development_Goal_8_DecentWork" class="rounded-md shadow-2xl">
-                </a>
+            <div class="col-span-2">
+                <div class="flex justify-center">
+                    <div class="flex items-center justify-center">
+                        <a href="https://tinyurl.com/ycy9s5kn">
+                            <img src="{{ asset('images/Sustainable_Development_Goal_08DecentWork.webp') }}"
+                                alt="Sustainable_Development_Goal_8_DecentWork" class="rounded-md shadow-2xl">
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
     {{-- end SDG focus --}}
     {{-- start partnership --}}
@@ -206,13 +212,13 @@
             </span>
         </h2>
     </div>
-    <div class="flex items-start w-full pl-8 mt-4">
+    <div class="flex items-start w-full px-32 pl-8 mt-4 mb-2">
         <h2 class="text-xl font-bold uppercase roboto-bold">
             <span class="inline-block pb-2 border-b-2 border-black">With</span>
             You
         </h2>
     </div>
-    <div class="flex bg-[#FCF6F6] p-8">
+    <div class="flex bg-[#FCF6F6] p-8 rounded-lg">
         <div class="flex-col flex-grow w-1/2">
             <p class="mt-2 roboto-regular">To drive forward
                 our mission within the IGNITE program, we
