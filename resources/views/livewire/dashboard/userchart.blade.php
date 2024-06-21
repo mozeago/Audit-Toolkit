b632a3ace1c43b348a98b5a331aa097f4357bf06
Moses Asiago <mozzeago@gmail.com>
2024-06-21 11:23:42 +0300

    dashboard bug fix

--- resources/views/livewire/dashboard/userchart.blade.php
+++ resources/views/livewire/dashboard/userchart.blade.php
@@ -593,7 +593,7 @@ new class extends Component {
                                     <path class="circle-bg" d="M18 2.0845
                             a 15.9155 15.9155 0 0 1 0 31.831
                             a 15.9155 15.9155 0 0 1 0 -31.831" />
-                                    <path class="circle" stroke-dasharray="{{ $auditScore }}, 100" d="M18 2.0845
+                                    <path class="circle" stroke-dasharray="{{ $securityScore }}, 100" d="M18 2.0845
                             a 15.9155 15.9155 0 0 1 0 31.831
                             a 15.9155 15.9155 0 0 1 0 -31.831" />
                                     <text x="18" y="20.35" class="font-bold percentage">{{ $securityScore }}
